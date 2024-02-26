a405c39e0f55a13fe78381137f37eee66d7df553
Owen Thompson <oxt3479@rit.edu>
2024-02-26 18:26:55 -0500

    Shader import of MVP

--- programs/cube-vertex.glsl
+++ programs/cube-vertex.glsl
@@ -3,12 +3,13 @@
 layout(location = 0) in vec3 vertexPosition_modelspace;
 layout(location = 1) in vec3 vertexColor;
 
+uniform mat4 MVP;
+
 out vec3 fragmentColor;
 
 void main(){
     // The color of each vertex will be interpolated
     // to produce the color of each fragment
     fragmentColor = vertexColor;
-    gl_Position.xyz = vertexPosition_modelspace;
-    gl_Position.w = 1.0;
+    gl_Position = MVP * vec4(vertexPosition_modelspace, 1.0);
 }
