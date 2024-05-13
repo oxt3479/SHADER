c1c886eea184cd4b25216de8583fbddbf743a90e
Owen Thompson <owen.thompson@unarlabs.com>
2024-05-12 17:51:47 -0700

    Minimal spell subroutine implementation

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,13 @@ int main() {
     PlayerContext player_context;    
     player_context.linkPlayerCellVAOs();
     
-    Uniforms* uniforms  = getUniforms(window);
+    Uniforms* uniforms = getUniforms(window);
     
     float time;
-    GLint U_RESOLUTION, U_MOUSE, U_SCROLL, U_TIME, U_CAMERA, U_WORLD;
+    GLuint  U_RESOLUTION, U_MOUSE, U_SCROLL, U_TIME, U_CAMERA, U_WORLD, \
+            U_SPELL_LIFE, U_CAST_LIFE, U_SPELL_FOCUS, U_SPELL_HEAD;
     CameraMats mats;
+    SpellLog spells;
 
     while (!glfwWindowShouldClose(window)) {
         if (uniforms->loading) {
@@ -30,16 +32,19 @@ int main() {
             U_TIME        = glGetUniformLocation(shader_prog.ID, "u_time");
             U_CAMERA      = glGetUniformLocation(shader_prog.ID, "CAMERA");
             U_WORLD       = glGetUniformLocation(shader_prog.ID, "WORLD");
+            U_CAST_LIFE   = glGetUniformLocation(shader_prog.ID, "CAST_LIFE");
+            U_SPELL_LIFE  = glGetUniformLocation(shader_prog.ID, "SPELL_LIFE");            
+            U_SPELL_FOCUS = glGetUniformLocation(shader_prog.ID, "SPELL_FOCUS");
+            U_SPELL_HEAD  = glGetUniformLocation(shader_prog.ID, "SPELL_HEAD");
 
             addUniformRGBATexture(shader_prog.ID, "rgbTexture", \
                 TEXTURE_DIR "/tile_floor_a.png", 0);
             addUniformRGBATexture(shader_prog.ID, "depthTexture", \
                 TEXTURE_DIR "/disp_test.png", 1);
 
-            time = glfwGetTime();
-            uniforms->last_time = time;
-            uniforms->loading = false;
-            uniforms->player_context = &player_context;
+            uniforms->last_time         = glfwGetTime();
+            uniforms->loading           = false;
+            uniforms->player_context    = &player_context;
             
             glEnable(GL_DEPTH_TEST);
             glDepthFunc(GL_LESS);
@@ -57,12 +62,21 @@ int main() {
         glUniform1f(U_SCROLL,       uniforms->scroll);
         glUniform1f(U_TIME, time);
         
-        getCameraMats(window, mats);
+        accountSpells(uniforms, spells);
 
-        glUniformMatrix4fv(U_CAMERA, 1, GL_FALSE, \
-            &(mats.Projection*mats.View)[0][0]);
-        glUniformMatrix4fv(U_WORLD, 1, GL_FALSE, \
-            &mats.Model[0][0]);
+        glUniform1f(U_CAST_LIFE,    spells.cast_life[spells.active_spell]);
+        glUniform1f(U_SPELL_LIFE,   spells.spell_life[spells.active_spell]);
+        glUniform3f(U_SPELL_FOCUS,  spells.spell_focus[spells.active_spell].x,
+                                    spells.spell_focus[spells.active_spell].y,
+                                    spells.spell_focus[spells.active_spell].z);
+        glUniform3f(U_SPELL_HEAD,   spells.spell_head[spells.active_spell].x,
+                                    spells.spell_head[spells.active_spell].y,
+                                    spells.spell_head[spells.active_spell].z);
+
+        accountCameraControls(uniforms, mats);
+
+        glUniformMatrix4fv(U_CAMERA, 1, GL_FALSE, &(mats.Projection*mats.View)[0][0]);
+        glUniformMatrix4fv(U_WORLD,  1, GL_FALSE, &(mats.Model)[0][0]);
         
         player_context.drawPlayerCellVAOs();
 
